de7a7625feff23e14b9d7b27be561155e223e895
Vinodh <vinodhravichandran@gmail.com>
2017-11-16 10:18:36 +0530

    upd
    updated

--- truncate_tables.sql
+++
@@ -1,1 +0,0 @@
-truncate table api_orders; truncate table api_orderitems; truncate table api_customers; truncate table api_products; truncate table api_categories; truncate table api_productcategories;
